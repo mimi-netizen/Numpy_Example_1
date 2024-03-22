ad23d550cb7bb7950ee785e570083a4584ca7617
Celyne <dianakydd2@gmail.com>
2024-03-22 11:05:58 +0300

    added array methods

---
+++ array_methods.py
@@ -0,0 +1,12 @@
+import numpy as np
+ar = np.array([1,2,3,4,5,670,78,50])
+#print(ar)
+#ndarray.reshape
+#ndarray.sum
+#ndarray.mean
+#ndarray.min
+#ndarray.max
+#ndarray.argmax
+#ndarray.argmin
+
+print(ar.reshape(2,4)) #will print 2 rows 4 columns
--- indexing_Slicing.py
+++ indexing_Slicing.py
@@ -1,3 +1,13 @@
 import numpy as np
 ar = np.array([1,2,3,45,6,57])
 print(ar[0])
+
+ar = np.array(
+    [
+        [1,3],
+        [6,4]
+    ]
+)
+print(ar)
+print(ar[0])
+print(ar[0][1]) #accesses first row second column
